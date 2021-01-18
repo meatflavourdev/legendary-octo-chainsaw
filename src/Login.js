7192276fe1ead60518339065c71d2d76a26491bd
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-01-17 22:20:07 -0800

    Messing around

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,28 @@
+import { createClient } from "@supabase/supabase-js";
+import { useEffect } from "react";
+const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY);
+
 function Login() {
+  let thisuser = '';
+  useEffect(() => {
+    async function dostuff() {
+      let { user, error } = await supabase.auth.signIn({
+        email: 'zenimus@gmail.com',
+        password: 'password'
+      })
+      console.log(user)
+      console.log(error)
+    }
+
+    dostuff()
+
+  }, [])
+
+  thisuser = supabase.auth.user()
   return (
     <div>
       <h2>Login</h2>
+      <h3>user: {thisuser.email}</h3>
     </div>
   );
 }
