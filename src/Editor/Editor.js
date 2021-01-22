5e61ad1a8a92582b4459b6353441736034a891a2
Nik S <nsofianos@hotmail.com>
2021-01-21 22:25:05 -0800

    remove footer crap from editor

--- src/Editor/Editor.js
+++ src/Editor/Editor.js
@@ -1,20 +1,9 @@
-import { useParams } from "react-router-dom";
 import MenuAppBar from "../components/MenuAppBar";
 
 function Editor() {
-  let { doc_id } = useParams();
-  let doc = ''
-  if (doc_id) {
-     doc = <h3>Document ID: {doc_id}</h3>;
-  } else {
-    doc = <h3>No document is open</h3>;
-  }
+
   return (
     <div>
-      <div className="info">
-        <h2>Editor</h2>
-        {doc}
-      </div>
       <MenuAppBar />
     </div>
   );
--- src/Editor/ProviderFlow.js
+++ src/Editor/ProviderFlow.js
@@ -23,34 +23,7 @@ const initialElements = [
   { id: 'provider-e3-4', source: 'provider-3', target: 'provider-4', animated: true },
 ];
 
-function Footer() {
-  const nodes = useStoreState((store) => store.nodes);
-  const transform = useStoreState((store) => store.transform);
-  const setSelectedElements = useStoreActions((actions) => actions.setSelectedElements);
-  const selectAll = () => {
-    setSelectedElements(nodes.map((node) => ({ id: node.id, type: node.type })));
-  };
-  return (
-    <aside className="flowInfoFooter">
-      <div className="description">
-        This is an example of how you can access the internal state outside of the ReactFlow component.
-      </div>
-      <div className="title">Zoom & pan transform</div>
-      <div className="transform">
-        [{transform[0].toFixed(2)}, {transform[1].toFixed(2)}, {transform[2].toFixed(2)}]
-      </div>
-      <div className="title">Nodes</div>
-      {nodes.map((node) => (
-        <div key={node.id}>
-          Node {node.id} - x: {node.__rf.position.x.toFixed(2)}, y: {node.__rf.position.y.toFixed(2)}
-        </div>
-      ))}
-      <div className="selectall">
-        <button onClick={selectAll}>select all nodes</button>
-      </div>
-    </aside>
-  );
-}
+
 
 const ProviderFlow = () => {
   const [elements, setElements] = useState(initialElements);
@@ -71,7 +44,6 @@ const ProviderFlow = () => {
             <Background variant="dots" gap={15} size={0.5} />
           </ReactFlow>
         </div>
-        <Footer />
       </ReactFlowProvider>
     </div>
   );
