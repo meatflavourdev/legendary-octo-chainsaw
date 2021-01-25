2db4e9f96b4e0ae718898a867ae15a53edd484d8
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-01-25 15:07:45 -0800

    ScreenBlock Node Type

--- src/Editor/nodeTypes/ScreenBlockNode.js
+++ src/Editor/nodeTypes/ScreenBlockNode.js
@@ -3,52 +3,46 @@ import { Handle } from "react-flow-renderer";
 import "./nodestyles.css";
 import classNames from "classnames";
 
-export default function ShapeNode({ data }) {
+export default function ScreenBlockNode({ style, data }) {
   // So we can check for all node elements easily
   data.isNode = true;
 
   const nodeClasses = classNames({
-    "node-shapenode": true,
-    filled: data.fillStyle === "filled",
-    outlined: data.fillStyle === "outlined",
-    dashed: data.fillStyle === "dashed",
-    dark: data.fillColor === "dark",
-    light: data.fillColor === "light",
-    red: data.fillColor === "red",
-    green: data.fillColor === "green",
-    blue: data.fillColor === "blue",
-    purple: data.fillColor === "purple",
+    "node-screenblock": true,
   });
 
+
+  const screenBlockStyle = `background-image: url(/screenblocks/page-${data.screenBlockID ? data.screenBlockID.toString().padStart(2, "0") : '01'}.svg)`;
+
   return (
-    <div className={nodeClasses}>
+    <div style={screenBlockStyle} className={nodeClasses}>
       <Handle
         type="source"
-        className="react-flow__handle-shapenode"
+        className="react-flow__handle-screenblocknode"
         position="top"
         onConnect={(params) => console.log("handle onConnect", params)}
         id="a"
       />
       <Handle
         type="source"
-        className="react-flow__handle-shapenode"
+        className="react-flow__handle-screenblocknode"
         position="left"
         onConnect={(params) => console.log("handle onConnect", params)}
         id="b"
       />
       <div>
-        <span className="shapenode-text">Custom Color</span>
+      <span className="screenblocknode-text"></span>
       </div>
       <Handle
         type="source"
-        className="react-flow__handle-shapenode"
+        className="react-flow__handle-screenblocknode"
         position="right"
         onConnect={(params) => console.log("handle onConnect", params)}
         id="c"
       />
       <Handle
         type="source"
-        className="react-flow__handle-shapenode"
+        className="react-flow__handle-screenblocknode"
         position="bottom"
         onConnect={(params) => console.log("handle onConnect", params)}
         id="d"
