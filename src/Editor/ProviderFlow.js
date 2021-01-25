9b75b953f7f528d462bd4ddb88adde3d03e17570
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-01-24 18:49:44 -0800

    Can modify the text for each node

--- src/Editor/ProviderFlow.js
+++ src/Editor/ProviderFlow.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect } from "react";
 import ReactFlow, {
   ReactFlowProvider,
   addEdge,
@@ -7,68 +7,153 @@ import ReactFlow, {
   useStoreState,
   useStoreActions,
   Background,
-} from 'react-flow-renderer';
-import './provider.css';
+} from "react-flow-renderer";
+import "./provider.css";
 import EditorToolbar from "./EditorToolbar";
-import EditNodes from './EditNodes';
-import AttributeToolbar from './AttributeToolbar';
+import EditNodes from "./EditNodes";
+import AttributeToolbar from "./AttributeToolbar";
+import "../UpdateNode/updatenode.css";
 
-const onElementClick = (event, element) => console.log('click', element);
-const onLoad = (reactFlowInstance) => console.log('flow loaded:', reactFlowInstance);
+const onLoad = (reactFlowInstance) =>
+  console.log("flow loaded:", reactFlowInstance);
 
 const initialElements = [
-  { id: 'provider-1', data: { label: 'Node 1' }, position: { x: 340, y: 150 }, type: 'input' },
-  { id: 'provider-2', data: { label: 'Node 2' }, position: { x: 150, y: 300 } },
-  { id: 'provider-3', data: { label: 'Node 3' }, position: { x: 550, y: 300 } },
-  { id: 'provider-4', data: { label: 'Node 4' }, position: { x: 550, y: 480 }, type: 'output' },
-  { id: 'provider-e1-2', source: 'provider-1', target: 'provider-2', animated: false, type: 'smoothstep' },
-  { id: 'provider-e1-3', source: 'provider-1', target: 'provider-3', animated: false, type: 'smoothstep' },
-  { id: 'provider-e3-4', source: 'provider-3', target: 'provider-4', animated: true, type: 'smoothstep' },
+  {
+    id: "provider-1",
+    data: { label: "Node 1" },
+    position: { x: 340, y: 150 },
+    type: "input",
+  },
+  { id: "provider-2", data: { label: "Node 2" }, position: { x: 150, y: 300 } },
+  { id: "provider-3", data: { label: "Node 3" }, position: { x: 550, y: 300 } },
+  {
+    id: "provider-4",
+    data: { label: "Node 4" },
+    position: { x: 550, y: 480 },
+    type: "output",
+  },
+  {
+    id: "provider-e1-2",
+    source: "provider-1",
+    target: "provider-2",
+    animated: false,
+    type: "smoothstep",
+  },
+  {
+    id: "provider-e1-3",
+    source: "provider-1",
+    target: "provider-3",
+    animated: false,
+    type: "smoothstep",
+  },
+  {
+    id: "provider-e3-4",
+    source: "provider-3",
+    target: "provider-4",
+    animated: true,
+    type: "smoothstep",
+  },
 ];
 const nodeTypes = {};
 
 const nodeDefaultValues = {
-  background: '#2D3A49',
-  color: '#FFF',
-  border: '0px'
-}
+  background: "#2D3A49",
+  color: "#FFF",
+  border: "0px",
+};
 const nodeShapes = {
   block: {
     ...nodeDefaultValues,
     width: 100,
-    padding: '20px',
-    borderRadius: '5px',
+    padding: "20px",
+    borderRadius: "5px",
   },
   terminator: {
     ...nodeDefaultValues,
-    borderRadius: '30px',
-    width: 120
-  }
+    borderRadius: "30px",
+    width: 120,
+  },
 };
 
 const ProviderFlow = () => {
   const [elements, setElements] = useState(initialElements);
-  const [nodeName, setNodeName] = useState('Node 1');
-  const onConnect = (params) => setElements((els) => addEdge({type: 'smoothstep', ...params}, els));
-  const onElementsRemove = (elementsToRemove) => setElements((els) => removeElements(elementsToRemove, els), console.log("REMOVED NODE"));
+  const [nodeName, setNodeName] = useState("TEST");
+  const [nodeBg, setNodeBg] = useState("#eee");
+  const [nodeHidden, setNodeHidden] = useState(false);
+  const [nodeId, setNodeId] = useState("");
+
+  const onElementClick = (event, element) => {
+    setNodeId(element.id);
+  };
+
+  const onConnect = (params) =>
+    setElements((els) => addEdge({ type: "smoothstep", ...params }, els));
+  const onElementsRemove = (elementsToRemove) =>
+    setElements(
+      (els) => removeElements(elementsToRemove, els),
+      console.log("REMOVED NODE")
+    );
 
   const getNodeId = () => `randomnode_${+new Date()}`;
-  const onAdd = useCallback((type, shape) => {
-    let randomNumber = (Math.floor(Math.random() * (60 - 20 + 1))+ 20)  * 10 ;
-    const newNode = {
-      type,
-      id: getNodeId(),
-      style: nodeShapes[shape],
-      data: { label: 'Added node' },
-      position: {
-        x: 300,
-        y: 300
-      },
-    };
+  const onAdd = useCallback(
+    (type, shape) => {
+      const newNode = {
+        type,
+        id: getNodeId(),
+        style: nodeShapes[shape],
+        data: { label: "Added node" },
+        position: {
+          x: 300,
+          y: 300,
+        },
+      };
 
-    setElements((els) => els.concat(newNode));
-  }, [setElements]);
+      setElements((els) => els.concat(newNode));
+    },
+    [setElements]
+  );
 
+  useEffect(() => {
+    setElements((els) =>
+      els.map((el) => {
+        if (el.id === nodeId) {
+          // it's important that you create a new object here in order to notify react flow about the change
+          el.data = {
+            ...el.data,
+            label: nodeName,
+          };
+        }
+
+        return el;
+      })
+    );
+  }, [nodeName, setElements]);
+
+  useEffect(() => {
+    setElements((els) =>
+      els.map((el) => {
+        if (el.id === "provider-1") {
+          // it's important that you create a new object here in order to notify react flow about the change
+          el.style = { ...el.style, backgroundColor: nodeBg };
+        }
+
+        return el;
+      })
+    );
+  }, [nodeBg, setElements]);
+
+  useEffect(() => {
+    setElements((els) =>
+      els.map((el) => {
+        if (el.id === "provider-1" || el.id === "provider-e1-2") {
+          // when you update a simple type you can just update the value
+          el.isHidden = nodeHidden;
+        }
+
+        return el;
+      })
+    );
+  }, [nodeHidden, setElements]);
 
   return (
     <div className="providerflow">
@@ -85,9 +170,31 @@ const ProviderFlow = () => {
             snapGrid={[10, 10]}
           >
             <Controls />
-            <AttributeToolbar/>
+            <AttributeToolbar />
+            <div className="updatenode__controls">
+              <label>label:</label>
+              <input
+                value={nodeName}
+                onChange={(evt) => setNodeName(evt.target.value)}
+              />
+
+              <label className="updatenode__bglabel">background:</label>
+              <input
+                value={nodeBg}
+                onChange={(evt) => setNodeBg(evt.target.value)}
+              />
+
+              <div className="updatenode__checkboxwrapper">
+                <label>hidden:</label>
+                <input
+                  type="checkbox"
+                  checked={nodeHidden}
+                  onChange={(evt) => setNodeHidden(evt.target.checked)}
+                />
+              </div>
+            </div>
             <EditorToolbar addNode={onAdd} />
-            <Background variant="dots" gap='20' color="#484848" />
+            <Background variant="dots" gap="20" color="#484848" />
           </ReactFlow>
         </div>
       </ReactFlowProvider>
