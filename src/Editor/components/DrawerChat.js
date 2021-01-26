f555cf78e50cfbc4a390c09dec12eea125816b5e
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-01-25 23:49:32 -0800

    Chat base UI

--- src/Editor/components/DrawerChat.js
+++ src/Editor/components/DrawerChat.js
@@ -10,6 +10,8 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import ChatList from './ChatList';
+import ChatInput from './ChatInput';
 
 const drawerWidth = 300;
 
@@ -47,23 +49,8 @@ export default function DrawerDocs({ openChat }) {
   >
     <div className={classes.drawerHeaderChat}></div>
     <Divider />
-    <List>
-      {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-        <ListItem button key={text}>
-          <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
-    </List>
-    <Divider />
-    <List>
-      {['All mail', 'Trash', 'Spam'].map((text, index) => (
-        <ListItem button key={text}>
-          <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
-    </List>
+      <ChatList />
+      <ChatInput />
   </Drawer>
   );
 };
