0712dc49c60c93d430c6f06b5e995851fa0d8b45
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-01-26 22:44:38 -0800

    New yJs state engine

--- src/Editor/ProviderFlow.js
+++ src/Editor/ProviderFlow.js
@@ -1,19 +1,15 @@
-import { useState, useCallback } from "react";
-import ReactFlow, {
-  ReactFlowProvider,
-  addEdge,
-  removeElements,
-  Controls,
-  Background,
-} from "react-flow-renderer";
-import "./provider.css";
-import EditorToolbar from "./EditorToolbar";
-import AttributeToolbar from "./AttributeToolbar";
-import ShapeNode from "./nodeTypes/ShapeNode";
-import ScreenBlockNode from "./nodeTypes/ScreenBlockNode";
+import React, { useState, useCallback, useRef } from 'react';
+import ReactFlow, { ReactFlowProvider, addEdge, removeElements, Controls, Background } from 'react-flow-renderer';
+import { useParams } from 'react-router-dom';
+import './provider.css';
+import EditorToolbar from './EditorToolbar';
+import AttributeToolbar from './AttributeToolbar';
+import ShapeNode from './nodeTypes/ShapeNode';
+import ScreenBlockNode from './nodeTypes/ScreenBlockNode';
 
-//Fires when flowchart has loaded
-const onLoad = (reactFlowInstance) => console.log("flow loaded:", reactFlowInstance);
+// Yjs Imports
+import * as Y from 'yjs';
+import { WebsocketProvider } from 'y-websocket';
 
 //Elements loaded on new doc
 import initialElements from './initialElements';
@@ -21,28 +17,88 @@ import initialElements from './initialElements';
 //Custom node types go here
 const nodeTypes = {
   ShapeNode,
-  ScreenBlockNode
+  ScreenBlockNode,
 };
 
-
 const ProviderFlow = () => {
-  const [elements, setElements] = useState(initialElements);
-  const [nodeName, setNodeName] = useState("Node 1");
+  // Get doc_id from router
+  let { doc_id } = useParams();
+
+  // Get yjs lib and create a reference to it
+  const ydoc = React.useRef(null);
+
+  // Get a state array for React Flow's elements array.
+  // We'll use this to update React Flow from Yjs
+  const [elements, setElements] = React.useState([]);
+
+  // A Reference to the currently dragging node ID
+  const nodeDraggingRef = React.useRef(null);
+  // A Reference to the currently dragging React Flow node's drag listener
+  const onDragRef = React.useRef((event) =>
+    // onDrag, update the yDoc with the node's current pixel space coords projected into project coords
+    ydoc.current
+      .getMap('node-' + nodeDraggingRef.current)
+      .set('position', reactFlowRef.current.project({ x: event.clientX, y: event.clientY }))
+  );
+
+  //Fires when React flow has loaded
+  const reactFlowRef = React.useRef(null);
+  const onLoad = (reactFlowInstance) => {
+    console.log('flow loaded:', reactFlowInstance)
+    reactFlowRef.current = reactFlowInstance;
+  };
+
+  // Handles React Flow Element removal (from Yjs shared type and thus, the state)
+  const onElementsRemove = (elementsToRemove) => setElements((els) => removeElements(elementsToRemove, els));
+
+  React.useEffect(() => {
+    ydoc.current = new Y.Doc({guid: doc_id});
+    console.log(`Loaded Y.Doc ID: ${doc_id}`, ydoc.current);
+    new WebsocketProvider('ws://localhost:5001', 'collab', ydoc.current);
+    const nodes = ydoc.current.getArray('all-nodes');
+    if (nodes.toArray().length === 0) {
+      initialElements.forEach((element, index) => {
+        const node = ydoc.current.getMap('node-' + element.id);
+        for (let [k, v] of Object.entries(element)) {
+          node.set(k, v);
+          node.observe((event, transaction) => {
+            if (event.keysChanged.has('position')) {
+              setElements((elements) =>
+                elements.map((element) => {
+                  if (element.id === event.currentTarget.get('id')) {
+                    return {
+                      ...element,
+                      position: event.currentTarget.get('position'),
+                    };
+                  }
+                  return element;
+                })
+              );
+            }
+          });
+        }
+        nodes.insert(index, [element]);
+      });
+      nodes.observe((event, transaction) => {});
+      setElements(nodes.toArray());
+    }
+  }, [doc_id]);
+
+  const onNodeDragStart = (event, node) => {
+    nodeDraggingRef.current = node.id;
+    document.addEventListener('mousemove', onDragRef.current);
+  };
+  const onNodeDragStop = (event, node) => {
+    nodeDraggingRef.current = node.id;
+    document.removeEventListener('mousemove', onDragRef.current);
+  };
 
   //Fires when you connect 2 handles
-  const onConnect = (params) =>
-    setElements((els) => addEdge({ type: "smoothstep", ...params }, els));
+  const onConnect = (params) => setElements((els) => addEdge({ type: 'smoothstep', ...params }, els));
 
-  //Fires when an element is deleted  
-  const onElementsRemove = (elementsToRemove) =>
-    setElements(
-      (els) => removeElements(elementsToRemove, els),
-      console.log("REMOVED NODE")
-    );
-  
   //Generates an ID for each new node
   const newNodeId = () => `randomnode_${+new Date()}`;
-  
+
   //CREATES NEW ELEMENTS
   const onAdd = useCallback(
     (type, customData) => {
@@ -51,7 +107,7 @@ const ProviderFlow = () => {
         type,
         id: newNodeId(),
         type,
-        data: { ...customData, label: "New node" },
+        data: { ...customData, label: 'New node' },
         position: {
           x: 300,
           y: 300,
@@ -62,10 +118,10 @@ const ProviderFlow = () => {
     },
     [setElements]
   );
-  
+
   //Fires when an element is clicked
   const onElementClick = (event, element) => {
-    console.log("click", element);
+    console.log('click', element);
   };
 
   return (
@@ -78,6 +134,8 @@ const ProviderFlow = () => {
             onConnect={onConnect}
             onElementsRemove={onElementsRemove}
             onLoad={onLoad}
+            onNodeDragStart={onNodeDragStart}
+            onNodeDragStop={onNodeDragStop}
             nodeTypes={nodeTypes}
             snapToGrid={true}
             snapGrid={[10, 10]}
@@ -85,9 +143,7 @@ const ProviderFlow = () => {
             connectionLineType="smoothstep"
           >
             <Controls />
-            <AttributeToolbar
-              setEls={setElements}
-            />
+            <AttributeToolbar setEls={setElements} />
             <EditorToolbar addNode={onAdd} />
             <Background variant="dots" gap="20" color="#484848" />
           </ReactFlow>
