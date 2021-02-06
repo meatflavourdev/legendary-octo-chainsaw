c0f788d991cf1fba10a9eeb6cc057af080fc387b
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-02-06 03:14:54 -0800

    Refactor routehandler

--- src/RouteHandler.js
+++ src/RouteHandler.js
@@ -3,50 +3,43 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
 } from "react-router-dom";
 
+import Landing from "./Landing/Landing";
 import Login from "./Login";
 import SignUp from "./SignUp";
-import { AuthProvider } from "./contexts/AuthContext"
-import EditorLayout from "./Editor/EditorLayout";
-import Error from "./Error";
+import Admin from "./Admin";
 import YjsTest from "./yjsSubscriber/YjsTest";
 import YjsFlowTest from "./yjsSubscriber/YjsFlowTest";
 import firebaseAuth from "./firebase/firebaseAuth";
 import cloudFirestore from "./firebase/cloudFirestore";
-import Landing from "./Landing/Landing";
-import firebase from "firebase";
-import "firebase/firestore";
-import "firebase/auth";
-import ProviderFlow from './Editor/ProviderFlow';
+import Error from "./Error";
+import Document from "./Editor/Document";
 
-import { useAuthState } from "react-firebase-hooks/auth";
-import Admin from "./Home";
-
-const auth = firebase.auth();
+import { AuthProvider } from "./contexts/AuthContext"
 
 function RouteHandler() {
-  const [user] = useAuthState(auth);
-
   return (
     <Router>
       <AuthProvider>
         <Switch>
           <Route exact path="/" component={Landing} />
-          <Route exact path="/app">
-            {/*!user ? <Redirect to="/Login" /> : <Editor />*/}
-            <EditorLayout />
-          </Route>
-          <Route exact path="/signup" component={SignUp} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
           <Route exact path="/admin" component={Admin} />
-          <Route exact path="/error" component={Error} />
+
           <Route exact path="/yjstest" component={YjsTest} />
           <Route exact path="/yjsflowtest" component={YjsFlowTest} />
           <Route exact path="/firebaseauth" component={firebaseAuth} />
           <Route exact path="/cloudfirestore" component={cloudFirestore} />
-          <Route path="/:doc_id" component={EditorLayout} />
+
+          <Route exact path="/error" component={Error} />
+
+          <Route exact path="/app">
+            <Document />
+          </Route>
+          <Route path="/:doc_id" component={Document} />
+
         </Switch>
       </AuthProvider>
     </Router>
