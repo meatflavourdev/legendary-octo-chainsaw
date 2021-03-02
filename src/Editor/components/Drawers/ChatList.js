1dee6ce92fccad17781f64551230a7344a8df3f9
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-03-01 17:25:03 -0800

    Identify the current user's own chat messages

--- src/Editor/components/Drawers/ChatList.js
+++ src/Editor/components/Drawers/ChatList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import clsx from  'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -23,21 +24,50 @@ const useStyles = makeStyles((theme) => ({
     display: 'inline',
   },
   listDivider: {
+    backgroundColor: '#e1e1e1',
     '&:first-child': {
       display: 'none',
     },
-    margin: 0,
+
+  },
+  item: {
+    paddingBottom: '1.5em',
+    marginTop: '-1px',
+    paddingLeft: '10px',
+  },
+  itemSelf: {
+    borderLeft: '7px solid #795cfc',
+    direction: 'rtl',
+     '& span > span': {
+      left: '0.9em',
+      right: 'inherit',
+    }
+  },
+  messageTitle: {
+    direction: 'ltr',
+    '& .MuiTypography-body1': {
+      fontWeight: '500',
+      overflow: 'hidden',
+      wordBreak: 'break-all',
+      display: '-webkit-box',
+      '-webkit-box-orient': 'vertical',
+      '-webkit-line-clamp': 1,
+    },
   },
   messageBody: {
     marginTop: '-3px',
   },
   messageTime: {
-    display: 'inline-block',
-    marginBottom: '3px',
+    position: 'absolute',
+    right: '0px',
+    color: 'rgb(103 58 183 / 95%)',
+    direction: 'ltr',
+    paddingTop: '0.5em',
   },
   messageText: {
     display: 'inline-block',
     marginTop: '0',
+    direction: 'ltr',
   },
 
 }));
@@ -46,7 +76,7 @@ const useStyles = makeStyles((theme) => ({
 export default function ChatList({ messages, chatListBottomRef }) {
   const classes = useStyles();
 
-  const { generateColor } = useAuth();
+  const { currentUser } = useAuth();
 
   // Scroll to bottom of chat list on change
   useEffect(() => {
@@ -57,26 +87,19 @@ export default function ChatList({ messages, chatListBottomRef }) {
 
   for (const message of messages) {
     chatMessages.push(
-      <Divider key={message.creationTime + '-divider'} className={classes.listDivider} variant="inset" component="li" />
+      <Divider id={message.id + '-divider'} key={message.id + '-divider'} className={classes.listDivider} variant="inset" component="li" />
     )
+    const itemClass = currentUser.uid === message.user.uid ? classes.itemSelf : classes.item;
     chatMessages.push(
-      <ListItem key={message.creationTime + '-message'} alignItems="flex-start">
+      <ListItem className={ clsx(classes.item, (currentUser.uid === message.user.uid) && classes.itemSelf ) } /* style={{borderLeftColor: currentUser.collabColor.color}} */ id={message.id + '-message'} key={message.id + '-message'} alignItems="flex-start">
           <ListItemAvatar>
           <Avatar alt={message.user.displayName} style={{backgroundColor: message.user.collabColor.color, color: message.user.collabColor.isLight ? '#000' : '#FFF' }}  src={message.user.photoURL} />
           </ListItemAvatar>
         <ListItemText
+            className={classes.messageTitle}
             primary={message.user.displayName}
             secondary={
               <span className={classes.messageBody}>
-                <Typography
-                  component="span"
-                  variant="caption"
-                  className={classes.messageTime}
-                  color="textSecondary"
-                >
-                  {ago(new Date(message.creationTime))}
-                </Typography>
-                <br/>
                 <Typography
                   component="span"
                   variant="body2"
@@ -84,6 +107,15 @@ export default function ChatList({ messages, chatListBottomRef }) {
                   color="textPrimary"
                   >
                   {message.message}
+                </Typography>
+                <br/>
+                <Typography
+                  component="span"
+                  variant="caption"
+                  className={classes.messageTime}
+                  color="textSecondary"
+                >
+                  {ago(new Date(message.creationTime))}
                 </Typography>
               </span>
             }
