7b78d555ddeb77d88a77e94b24b0ca83ae36eb8c
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-03-18 19:33:26 -0700

    Cleanup: Remove unused code

--- src/Editor/nodeTypes/CursorNode.js
+++ src/Editor/nodeTypes/CursorNode.js
@@ -4,40 +4,25 @@ import { hexToCSSFilter } from 'hex-to-css-filter';
 import { useAuth } from '../../contexts/AuthContext';
 import './nodestyles.css';
 
-// UUID generator
-const uuid62 = require('uuid62');
-
-// Get ID for the clipping path
-const clipID = `svg-clippath-${uuid62.v4()}`;
-
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
     height: '100%',
     pointerEvents: 'none',
     cursor: 'pointer',
-    display: props => props.thisUser,
+    display: props => props.display,
   },
   collabCursor: {
     position: 'absolute',
     top: '0',
     width: '24px',
     height: '24px',
-    filter: props => props.filterData.filter,
     pointerEvents: 'none',
-  },
-  svgClipPath: {
-    width: '0',
-    height: '0',
-    pointerEvents: 'none',
-  },
-  noMaxWidth: {
-    maxWidth: 'none',
   },
   tooltip: {
     position: 'absolute',
-    top: '18px',
-    left: '5px',
+    top: '17px',
+    left: '3.5px',
     pointerEvents: 'none',
   },
   tooltipBody: {
@@ -46,44 +31,20 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '0.625rem',
     maxWidth: 'none',
     wordWrap: 'break-word',
-    fontWeight: '500',
+    fontWeight: '400',
     lineHeight: '1.4em',
     borderRadius: '4px',
     whiteSpace: 'nowrap',
     backgroundColor: props => props.backgroundColor,
     color: props => props.color,
   },
-  tooltipArrow: {
-    top: '2px',
-    left: '14px',
-    width: '0.71em',
-    height: '1em',
-    marginTop: '4px',
-    marginLeft: '-0.71em',
-    marginBottom: '4px',
-    overflow: 'hidden',
-    position: 'absolute',
-    boxSizing: 'border-box',
-    background: 'none',
-    '&:before': {
-      transformOrigin: '100% 100%',
-      width: '100%',
-      height: '100%',
-      margin: 'auto',
-      content: '""',
-      display: 'block',
-      transform: 'rotate(45deg)',
-      backgroundColor: props => props.backgroundColor,
-    },
-  },
 }));
 
-export default function CursorNode({ data }) {
-
+export default function CursorNode({ id, data }) {
   const { clientID } = useAuth();
 
   const cssProps = {
-    thisUser: data.clientID === clientID ? 'none' : 'inherit',
+    display: data.clientID === clientID ? 'none' : 'inherit',
     backgroundColor: data.collabColor?.color || 'black', color: data.collabColor.isLight ? 'black' : 'white',
     filterData: hexToCSSFilter(data.collabColor?.color) || 'none',
   }
@@ -91,11 +52,10 @@ export default function CursorNode({ data }) {
 
   return (
     <div id={data.nodeKey} className={classes.root}>
-      <img src="./img/cursor/001-cursor-01-mask.svg" className={classes.collabCursor} alt="" />
+      <img src="./img/cursor/001-cursor-01-mask.svg" className={classes.collabCursor} alt="" style={{ filter: cssProps.filterData.filter.slice(0, -1) }} />
       <div className={classes.tooltip}>
         <div className={classes.tooltipBody}>
           {data.displayName}
-          {/* <span className={classes.tooltipArrow}></span> */}
         </div>
       </div>
     </div>
