814e96a96b37e55aad11a96a5b5a0cef3eca54c0
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-04-07 22:27:56 -0700

    :building_construction: :construction: Editor FSM 1st draft

--- src/Editor/editorMachine.js
+++ src/Editor/editorMachine.js
@@ -1,26 +1,5 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import ProviderFlow from './ProviderFlow';
-import DrawerDocs from './components/Drawer/DrawerDocs';
-import DrawerChat from './components/Drawer/DrawerChat';
-import EditorAppBar from './components/AppBar/EditorAppBar';
-import { useAuth } from '../contexts/AuthContext';
 import { Machine, assign } from "xstate";
-import { useMachine } from "@xstate/react";
 import { inspect } from "@xstate/inspect";
-
-import useYDoc from './hooks/useYDoc';
-
-const useStyles = makeStyles(() => ({
-  root: {
-    display: 'flex',
-    overflow: 'hidden',
-    width: '100vw',
-    height: '100vh',
-  },
-}));
 
 inspect({
   url: "https://statecharts.io/inspect",
@@ -143,39 +122,4 @@ const editorMachine = Machine({
   },
 });
 
-export default function Editor() {
-  // Get doc_id from router
-  let { doc_id } = useParams();
-
-  const [current, send] = useMachine(editorMachine, { devTools: true });
-
-  const classes = useStyles();
-  const [openDocs, setOpenDocs] = React.useState(false);
-  const [openChat, setOpenChat] = React.useState(false);
-
-  const { currentUser } = useAuth();
-
-  // Get a reference to Yjs yDoc, awareness, and the websocket sync state boolean
-  const [wsSync, yDoc, awarenessState, awarenessRef] = useYDoc(doc_id, currentUser);
-
-  // TODO: Move Document CRUD logic here
-
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <EditorAppBar
-        docName={doc_id}
-        openDocs={openDocs}
-        openChat={openChat}
-        setOpenDocs={setOpenDocs}
-        setOpenChat={setOpenChat}
-        wsSync={wsSync}
-        yDoc={yDoc}
-        awarenessState={awarenessState}
-      />
-      <DrawerDocs openDocs={openDocs} setOpenDocs={setOpenDocs} />
-      <ProviderFlow setOpenDocs={setOpenDocs} yDoc={yDoc} wsSync={wsSync} awarenessRef={awarenessRef} />
-      <DrawerChat openChat={openChat} yDoc={yDoc} wsSync={wsSync} />
-    </div>
-  );
-}
+export default editorMachine;
