5a6ca6e7718fb0982b2b1745729435170520b524
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-04-07 22:21:45 -0700

    :building_construction: useYArray and useMapArray

--- src/Editor/hooks/useYArray.js
+++ src/Editor/hooks/useYArray.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import * as Y from 'yjs';
 
-const useYArray = (doc, name) => {
-  const valueRef = React.useRef(doc.getArray(name));
-  const [state, setState] = React.useState(null);
+const useYArray = (yDoc, yArrayName) => {
+  const [yArrayState, setYArrayState] = React.useState([]);
+
   React.useEffect(() => {
-    valueRef.current.observe((event) => {
-      setState(valueRef.current.toArray());
-    });
-  }, []);
-  return {
-    value: state,
-    insertValue: (i, v) => valueRef.current.insert(i, [v]),
-    pushValue: (v) => valueRef.current.push([v]),
-  };
+    const yArray = yDoc.getArray(yArrayName);
+
+    const yArrayObserver = (yArrayEvent, transaction) => {
+
+      //console.log('yEvent: ', yEvent);
+
+/*       const changeIndex = yEvent.delta[0].retain;
+
+      if (yEvent.changes.added.size) {
+        const elements = yEvent.delta[1].insert;
+        console.log(`Elements to insert at index[${changeIndex}]: `, elements);
+      }
+      if (yEvent.changes.deleted.size) {
+        const elements = yEvent.delta[1].delete;
+        console.log(`Elements to delete at index[${changeIndex}]: `, elements);
+      } */
+      //if (yEvent instanceof Y.YArrayEvent) {
+      //const op = Object.keys(changesDelta[1]);
+      //const node = Object.values(changesDelta[1])[0];
+        //console.log(`Observed change -- index: ${index} op: ${op} node: `, node); */
+      //}
+      console.log('yArrayObserver: ', yArrayEvent, transaction);
+
+      requestAnimationFrame(() => {
+        console.log('yArray Conversion: ', yArray.toArray(), yArray.toJSON());
+        setYArrayState(yArray.toJSON());
+      });
+    };
+    yArray.observeDeep(yArrayObserver);
+
+    return () => yArray.unobserve(yArrayObserver);
+  }, [yDoc, yArrayName]);
+
+  return [yArrayState, setYArrayState];
 };
 
 export default useYArray;
